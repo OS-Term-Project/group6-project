6eeaada5817fb6155ef43684caf3d58204be4bad
davidkpognon <77710946+davidkpognon@users.noreply.github.com>
2023-12-14 21:03:56 -0500

    Update BackingStore.c

--- BackingStore.c
+++ BackingStore.c
@@ -9,31 +9,33 @@
 #define NUM_PAGES 256
 #define TLB_SIZE 16
 
-// TLB entry structure
+
 typedef struct {
     int page_number;
     int frame_number;
 } tlb_entry;
 
-// page table
+
 int page_table[NUM_PAGES];
 // physical memory
 char physical_memory[NUM_FRAMES][PAGE_SIZE];
-// TLB
+
 tlb_entry tlb[TLB_SIZE];
 
-int next_available_frame = 0; // activatesthe next available frame
+int next_available_frame = 0; 
 
 // function to handle page fault
 void handle_page_fault(int page_number, FILE *backing_store) {
-    // read the page from the backing store and store it in the next available frame
+    // read the page from the backing store 
     fseek(backing_store, page_number * PAGE_SIZE, SEEK_SET);
     fread(physical_memory[next_available_frame], sizeof(char), PAGE_SIZE, backing_store);
-    // update the page table with the new frame number
+    
+    // update 
     page_table[page_number] = next_available_frame;
-    // update the TLB
+    
     tlb[next_available_frame % TLB_SIZE].page_number = page_number;
     tlb[next_available_frame % TLB_SIZE].frame_number = next_available_frame;
+    
     // increment the next available frame
     next_available_frame++;
 }
@@ -43,8 +45,8 @@ int main() {
     FILE *backing_store = fopen("BACKING_STORE.bin", "rb");
 
     if (backing_store == NULL) {
-        fprintf(stderr, "Error: Could not open .\n");
-        return 1; // 
+        fprintf(stderr, "Error: Could not open \n");
+        return 1; 
     }
 
     // handle page fault for page 251
@@ -53,10 +55,10 @@ int main() {
     // access the memory located at frame 0 and offset 25 
     printf("The value at physical address %d\n", physical_memory[0][25]);
 
-    // close the backing store file
+
     if (fclose(backing_store) != 0) {
-        fprintf(stderr, "Error: Could not close .\n");
-        return 1; // 
+        fprintf(stderr, "Error: Could not close\n");
+        return 1;
     }
 
     return 0;
